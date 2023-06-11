dbafe7446342586ab73e64b95ad6ab3f145f8cd5
ryo <llh5432@naver.com>
2023-06-11 17:01:47 +0900

    Initial commit

---
+++ spring-api-custom-app/src/main/java/com/app/SpringApiCustomAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiCustomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiCustomAppApplication.class, args);
+	}
+
+}
---
+++ spring-api-custom-app/src/main/java/com/app/api/health/controller/HealthCheckController.java
@@ -0,0 +1,30 @@
+package com.app.api.health.controller;
+
+import com.app.api.health.dto.HealthCheckResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    private final Environment environment;
+
+    @GetMapping("/health")
+    public ResponseEntity<HealthCheckResponse> healthCheck() {
+
+        HealthCheckResponse healthCheckResponse = HealthCheckResponse.builder()
+                .health("ok")
+                .activeProfiles(Arrays.asList(environment.getActiveProfiles()))
+                .build();
+        return ResponseEntity.ok(healthCheckResponse);
+    }
+
+}
